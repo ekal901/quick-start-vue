3a8668c4cfbefc0863ab1583f5f1ce81acacdb40
kdh <ekal.kdh@gmail.com>
2019-06-02 16:41:03 +0900

    Recursive component content

--- ch9/dynamic-component/src/components/About.vue
+++ ch9/dynamic-component/src/components/About.vue
@@ -3,10 +3,67 @@
   <div>
     <h1>About</h1>
     <h3>{{ (new Date()).toTimeString() }}</h3>
+    <h4>조직도</h4>
+    <tree :subs="orgcharts"></tree>
   </div>
 </template>
 <script>
+import Tree from "./Tree";
 export default {
-  name: "about"
+  name: "about",
+  components: {
+    Tree
+  },
+  data() {
+    return {
+      orgcharts: [
+        {
+          name: "OpenSG",
+          type: "company",
+          subs: [
+            {
+              name: "SI 사업부",
+              type: "division",
+              subs: [
+                { name: "SI 1팀", type: "team" },
+                { name: "SI 2팀", type: "team" }
+              ]
+            },
+            {
+              name: "BI 사업부",
+              type: "division",
+              subs: [
+                { name: "BI 1팀", type: "team" },
+                { name: "BI 2팀", type: "team" },
+                { name: "BI 3팀", type: "team" }
+              ]
+            },
+            {
+              name: "솔루션 사업부",
+              type: "division",
+              subs: [
+                { name: "ESM팀", type: "team" },
+                { name: "MTS팀", type: "team" },
+                { name: "ASF팀", type: "team" }
+              ]
+            },
+            { name: "총무팀", type: "team" },
+            { name: "인사팀", type: "team" }
+          ]
+        }
+      ]
+    };
+  }
 };
 </script>
+<style>
+li.company {
+  color: dodgerblue;
+}
+li.division {
+  color: lightcoral;
+}
+li.team {
+  color: mediumorchid;
+}
+</style>
---
+++ ch9/dynamic-component/src/components/Tree.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li v-for="s in subs" :class="s.type" :key="s.name">
+      {{s.name}}
+      <tree :subs="s.subs"></tree>
+      <!-- 재귀 호출 부분 -->
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "tree",
+  props: ["subs"]
+};
+</script>
