60a7c208376a9d4d101f18cfaf8c37c34be1a681
김담희 <kdh@wooam.com>
2019-06-04 15:24:56 +0900

    vuex action

---
+++ ch11/todolistapp-action/src/store/index.js
@@ -0,0 +1,51 @@
+// src/store/index.js
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Constant from '../Constant'
+
+Vue.use(Vuex); // Vuex를 전역에서 사용하도록 작성
+
+const store = new Vuex.Store({
+  state: {
+    todolist: [
+      { id: 1, todo: "Watching Movie", done: false },
+      { id: 2, todo: "Walking", done: true },
+      { id: 3, todo: "ES6 Study", done: false },
+      { id: 4, todo: "Basketball Game", done: false }
+    ]
+  },
+  mutations: {
+    // state : 기존 상태 정보
+    // payload : 상태를 변경할 때 필요한 정보 (새로 변경할 정보)
+    [Constant.ADD_TODO]: (state, payload) => {
+      if (payload.todo !== "") {
+        state.todolist.push({ id: state.todolist.length + 1, todo: payload.todo, done: false });
+      }
+    },
+    [Constant.DONE_TOGGLE]: (state, payload) => {
+      let index = payload.id - 1;
+      state.todolist[index].done = !state.todolist[index].done;
+    },
+    [Constant.DELETE_TODO]: (state, payload) => {
+      let index = payload.id - 1;
+      state.todolist.splice(index, 1);
+    }
+  },
+  actions: {
+    // 구조 분해 할당을 이용한 Action
+    [Constant.ADD_TODO]: ({ commit }, payload) => {
+      console.log("###AddToDo!!!", payload);
+      commit(Constant.ADD_TODO, payload);
+    },
+    [Constant.DELETE_TODO]: (state, payload) => {
+      console.log("###DeleteToDo!!!", payload);
+      state.commit(Constant.DELETE_TODO, payload);
+    },
+    [Constant.DONE_TOGGLE]: (state, payload) => {
+      console.log("###DoneToggle!!!", payload);
+      state.commit(Constant.DONE_TOGGLE, payload);
+    }
+  }
+})
+
+export default store;
