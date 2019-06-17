d004de76c2cbd6c8cffb52f163e5dc4c2cfa1804
kdh <ekal.kdh@gmail.com>
2019-06-17 11:51:38 +0900

    dynamic route is done

---
+++ ch12/02.dynamic-route/src/components/Contact.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+    <div class="wrapper">
+      <div class="box" v-for="c in contacts" :key="c.no">
+        <router-link v-bind:to="'/contact/'+c.no">{{c.name}}</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import contactlist from "../ContactList";
+export default {
+  name: "Contact",
+  data() {
+    return {
+      contacts: contactlist.contacts
+    };
+  }
+};
+</script>
+
+<style>
+.wrapper {
+  background-color: #fff;
+  clear: both;
+  display: table;
+}
+.box {
+  float: left;
+  background-color: lavender;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 3px;
+  text-align: center;
+  font-size: 120%;
+  width: 100px;
+  font-weight: bold;
+}
+a:link,
+a:visited {
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+</style>
---
+++ ch12/02.dynamic-route/src/components/ContactByNo.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>Contact Detail</h1>
+    <div>
+      <table class="detail table table-bordered">
+        <tbody>
+          <tr class="active">
+            <td>Serial Number</td>
+            <td>{{contact.no}}</td>
+          </tr>
+          <tr class="active">
+            <td>Name</td>
+            <td>{{contact.name}}</td>
+          </tr>
+          <tr class="active">
+            <td>Tel</td>
+            <td>{{contact.tel}}</td>
+          </tr>
+          <tr class="active">
+            <td>Address</td>
+            <td>{{contact.address}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import contactlist from "../ContactList";
+export default {
+  name: "ContactByNo",
+  data() {
+    return {
+      no: 0,
+      contacts: contactlist.contacts
+    };
+  },
+  created() {
+    this.no = this.$route.params.no;
+  },
+  computed: {
+    contact() {
+      var no = this.no;
+      var arr = this.contacts.filter(item => {
+        return item.no == no;
+      });
+      if (arr.length == 1) {
+        return arr[0];
+      } else {
+        return {};
+      }
+    }
+  }
+};
+</script>
+
+<style>
+table.detail {
+  width: 400px;
+}
+</style>
