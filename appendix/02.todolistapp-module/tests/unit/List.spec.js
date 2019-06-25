57184ebc5262f6d2aefdd4b2debde20297c165b5
kdh <ekal.kdh@gmail.com>
2019-06-25 17:38:45 +0900

    이벤트 시뮬레이션 완료

--- appendix/02.todolistapp-module/tests/unit/List.spec.js
+++ appendix/02.todolistapp-module/tests/unit/List.spec.js
@@ -4,34 +4,19 @@ import { mount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Vue from 'vue'
 import store from '@/store'
-import List from '@/components/List.vue'
-// Constant 파일 import
-import Constant from '@/Constant'
+// Constant 삭제, List.vue 컴포넌트 삭제, ToDoList.vue 컴포넌트 import
+import ToDoList from '@/components/ToDoList.vue'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-describe('List.vue', () => {
+describe('ToDoList.vue', () => {
   let wrapper;
 
-  // const wrapper = mount(List, { store, localVue })를
-  // before() 이벤트 훅으로 공유
   before(() => {
-    // 테스트 스위트 시작 전에, wrapper 객체를 하나 만들어 두 테스트에서 공유
-    wrapper = mount(List, { store, localVue })
+    // mount하는 컴포넌트 ToDoList.vue로 변경
+    wrapper = mount(ToDoList, { store, localVue })
     console.log("before: 테스트 스위트 전체의 시작 전에 실행")
-  })
-
-  after(() => {
-    console.log("after: 테스트 스위트 전체의 종료 후에 실행")
-  })
-
-  beforeEach(() => {
-    console.log("beforeEach: 각 테스트의 시작 전에 실행")
-  })
-
-  afterEach(() => {
-    console.log("afterEach: 각 테스트의 종료 후에 실행")
   })
 
   it('초기 렌더링 화면 테스트', (done) => {
@@ -43,18 +28,28 @@ describe('List.vue', () => {
       .catch(done)
   })
 
-  it('새로운 todo 추가후 목록 확인', (done) => {
-    // store의 dispatch 메서드를 이용해, 추가 액션을 일으킨 후에
-    // List.vue 컴포넌트가 렌더링하는 목록의 개수를 확인하는 테스트
-    wrapper.vm.$store.dispatch(Constant.ADD_TODO, { todo: '스쿼시' })
-    wrapper.vm.$store.dispatch(Constant.ADD_TODO, { todo: '수영' })
+  it('클릭이벤트 후 렌더링 결과 확인', (done) => {
+    // 입력 값을 TodoList의 첫번째 자식 컴포넌트인 InputToDo 컴포넌트의 
+    // 로컬 데이터에 새로운 todo 입력
+    // 1. InputToDo.vue의 data()에 todo를 변경
+    wrapper.vm.$children[0].$data.todo = "피겨강습";
 
+    // 2. 이벤트 객체 생성
+    const evtClick = new window.Event('click');
+    // 3. 클릭할 버튼 검색 (span태그 중에 클래스명이 addbutton인 버튼)
+    var addbutton = wrapper.vm.$el.querySelector('span.addbutton');
+    // dispatchEvent로 클릭 이벤트를 일으킨다.
+    addbutton.dispatchEvent(evtClick);
+    // Vue 인스턴스에서 상태의 변경을 감지할 수 있도록
+    // 감시자(watcher)를 구동
+    wrapper.vm._watcher.run();
+
+    // nextTick()을 이용해 DOM 업데이트 사이클이 완료된 후에 결과를 확인
     Vue.nextTick()
       .then(() => {
         var list = wrapper.vm.$el.querySelectorAll('li');
-        expect(list[list.length - 1].textContent).to.contain('수영')
-        expect(list[list.length - 2].textContent).to.contain('스쿼시')
-        expect(list.length).to.equal(6)
+        expect(list[list.length - 1].textContent).to.contain('피겨강습')
+        expect(list.length).to.equal(5)
         done()
       })
       .catch(done)
