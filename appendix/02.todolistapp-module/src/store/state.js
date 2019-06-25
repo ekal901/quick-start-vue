35b15a19976a76c9b9e48717b9a674b2b804e087
kdh <ekal.kdh@gmail.com>
2019-06-25 16:37:27 +0900

    unit-mocha plugin is installed

---
+++ appendix/02.todolistapp-module/src/store/state.js
@@ -0,0 +1,9 @@
+// src/store/state.js
+export default {
+  todolist: [
+    { id: 1, todo: "Watching Movie", done: false },
+    { id: 2, todo: "Walking", done: true },
+    { id: 3, todo: "ES6 Study", done: false },
+    { id: 4, todo: "Basketball Game", done: false }
+  ]
+}
